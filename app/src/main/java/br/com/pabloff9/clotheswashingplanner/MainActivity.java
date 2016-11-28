eb38daf2af90810d13d5b6b3907b4237e9aa0781
Pablo Almeida de Andrade <pabloalmeidaff9@gmail.com>
2016-11-28 07:40:03 -0200

    First commit: a FAB and a recyclerview

---
+++ app/src/main/java/br/com/pabloff9/clotheswashingplanner/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.pabloff9.clotheswashingplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
